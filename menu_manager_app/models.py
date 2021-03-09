ef8f5e99e17320a0bfba8625feddb457f1779e21
Adrian Kodjasz <adrian.kodjasz@gmail.com>
2021-03-09 21:43:51 +0100

    models.py - added MenuManager & string repr for dish

--- menu_manager_app/models.py
+++ menu_manager_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models import Count
 
 
 class User(AbstractUser):
@@ -15,10 +16,19 @@ class Dish(models.Model):
     updated = models.DateTimeField(auto_now=True)
     vegetarian = models.BooleanField(blank=False, default=False)
 
+    def __str__(self):
+        return f"Dish name: {self.name}. Description: {self.description}. Price: {self.price}"
+
+
+class MenuManager(models.Manager):
+    def get_queryset(self):
+        return super(MenuManager, self).get_queryset().annotate(total_dishes=Count('dishes'))
+
 
 class Menu(models.Model):
     name = models.CharField(unique=True, max_length=80)
-    dishes = models.ManyToManyField(Dish)
+    dishes = models.ManyToManyField(Dish, blank=True)
     description = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    objects = MenuManager()
