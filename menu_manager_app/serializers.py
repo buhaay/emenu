06c21590a890e5260b49b2ffc639bad40e43c2a5
Adrian Kodjasz <adrian.kodjasz@gmail.com>
2021-03-09 22:04:04 +0100

    serializers.py - override to_representation method for dish serializer details

--- menu_manager_app/serializers.py
+++ menu_manager_app/serializers.py
@@ -1,13 +1,19 @@
 from rest_framework import serializers
 from .models import Menu, Dish
 
-class MenuSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Menu
-        fields = '__all__'
 
 class DishSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dish
         fields = '__all__'
 
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        representation = super(MenuSerializer, self).to_representation(instance)
+        representation['dishes'] = DishSerializer(instance.dishes.all(), many=True).data
+        return representation
